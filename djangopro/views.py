83ead1b66fa3fa3d82127fc017c650c978c5959b
blacksheep107 <blacksheep107@126.com>
2022-04-02 16:12:04 +0800

    kmeans, slic origin code

--- djangopro/views.py
+++ djangopro/views.py
@@ -4,7 +4,7 @@ import os
 import base64
 import cv2
 import numpy as np
-from . import kmeans
+from . import segregation
 from PIL import Image
 def hello(request):
     if request.method == 'GET':
@@ -19,7 +19,7 @@ def hello(request):
         print(os.path.join(os.getcwd(), 'static/images', "img.png"))
         img.save(os.path.join(os.getcwd(), 'static/images', "img.png"))
         print('save ok')
-        inertia = kmeans.main(os.path.join(os.getcwd(), 'static/images', "img.png"),
+        inertia = segregation.main(os.path.join(os.getcwd(), 'static/images', "img.png"),
                     postObj['type'],
                     postObj['step'],
                     postObj['angle'],
@@ -34,6 +34,7 @@ def hello(request):
             'slic_result': False,
             'hsvMask': False,
             'result': False,
+            'result_noslic':False,
             'type': postObj['type'],
             'slic_num': postObj['slic_num'],
             'compactness_num': postObj['compactness_num'],
@@ -42,9 +43,9 @@ def hello(request):
             'gabor_fre': postObj['gabor_fre'],
             'inertia': inertia
         }
-        contextArr = ['origin_img', 'noplant_img', 'filt_img', 'GLCM_Entropy','slic_result', 'hsvMask', 'result']
+        contextArr = ['origin_img', 'noplant_img', 'filt_img', 'GLCM_Entropy','slic_result', 'hsvMask', 'result', 'result_noslic']
         pathArr = [
-            'img.png', 'noplant.png', 'filt_imag.png', 'GLCM_Features.png','slic_result.png', 'hsvMask.png', 'result.png'
+            'img.png', 'noplant.png', 'filt_imag.png', 'GLCM_Features.png','slic_result.png', 'hsvMask.png', 'result.png', 'result_noslic.png'
         ]
         for i in range(len(pathArr)):
             path = os.path.join(os.getcwd(), 'static/images', pathArr[i])
@@ -55,3 +56,35 @@ def hello(request):
                     img_stream = base64.b64encode(img_stream)
                     context[contextArr[i]] = "data:image/jpg;base64," + str(img_stream)[2: -1]
         return render(request, 'test.html', context)
+
+def onlykmeans(request):
+    if request.method == 'GET':
+        return render(request, 'onlykmeans.html')
+    elif request.method == 'POST':
+        imgfile = request.FILES.get('img')
+        img = Image.open(imgfile)
+        postObj = request.POST
+        print(postObj)
+        print(os.path.join(os.getcwd(), 'static/images', "img.png"))
+        img.save(os.path.join(os.getcwd(), 'static/images', "img.png"))
+        print('save ok')
+        inertia = segregation.onlykmeans(os.path.join(os.getcwd(), 'static/images', "img.png"),
+                    postObj['type'])
+        context = {
+            'origin_img': False,
+            'noplant_img': False,
+            'inertia': inertia
+        }
+        contextArr = ['origin_img', 'noplant_img']
+        pathArr = [
+            'img.png', 'noplant.png'
+        ]
+        for i in range(len(pathArr)):
+            path = os.path.join(os.getcwd(), 'static/images', pathArr[i])
+            img_stream = ''
+            if os.path.exists(path):
+                with open(path, 'rb') as img_f:
+                    img_stream = img_f.read()
+                    img_stream = base64.b64encode(img_stream)
+                    context[contextArr[i]] = "data:image/jpg;base64," + str(img_stream)[2: -1]
+        return render(request, 'onlykmeans.html', context)
